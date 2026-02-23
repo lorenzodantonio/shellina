bbedfa1737e753199df6b814e8f3841584f5ff9c
Lorenzo <lorenzodantonio1995@gmail.com>
2026-02-24 00:46:09 +0100

    sketch ast and parse pipe

--- parser.c
+++ parser.c
@@ -4,60 +4,152 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct parser *parser_new(char *line) {
-  struct parser *p = malloc(sizeof(*p));
-  p->line = line;
-  p->cursor = line;
-  p->state = STATE_NORMAL;
-  return p;
+struct token *token_new(enum token_type type, char *str) {
+  struct token *t = malloc(sizeof(*t));
+  t->str = strdup(str);
+  t->type = type;
+  return t;
 }
 
-void parser_free(struct parser *p) {
-  //
-  free(p);
+void token_free(struct token *t) {
+  free(t->str);
+  free(t);
 }
 
-char *parser_next(struct parser *p) {
-  while (isspace(*p->cursor)) {
-    p->cursor++;
+void token_list_push(struct token_list *lst, struct token *t) {
+  if (lst->count == lst->capacity) {
+    size_t newcap = lst->capacity * 2;
+    lst->tokens = realloc(lst->tokens, sizeof(*lst->tokens) * newcap);
+    lst->capacity = newcap;
   }
 
-  if (!*p->cursor) {
+  lst->tokens[lst->count++] = t;
+}
+
+struct lexer *lexer_new(char *line) {
+  struct lexer *l = malloc(sizeof(*l));
+  l->line = line;
+  l->cursor = line;
+  l->state = STATE_NORMAL;
+  return l;
+}
+
+void lexer_free(struct lexer *l) {
+  //
+  free(l);
+}
+
+struct token *lexer_next(struct lexer *l) {
+  while (isspace(*l->cursor)) {
+    l->cursor++;
+  }
+
+  if (!*l->cursor) {
     return NULL;
   }
 
-  int tlen = 0;
-  char *token = malloc(512);
-  while (*p->cursor) {
-    if (*p->cursor == '\"') {
-      p->state = (p->state == STATE_NORMAL) ? STATE_IN_QUOTE : STATE_NORMAL;
-      p->cursor++;
+  if (*l->cursor == '|') {
+    l->cursor++;
+    return token_new(TOKEN_PIPE, "|");
+  }
+
+  char tstr[4096];
+  size_t tlen = 0;
+
+  while (*l->cursor) {
+    if (*l->cursor == '\"') {
+      l->state = (l->state == STATE_NORMAL) ? STATE_IN_QUOTE : STATE_NORMAL;
+      l->cursor++;
       continue;
     }
 
-    if (p->state == STATE_NORMAL && isspace(*p->cursor)) {
+    if (l->state == STATE_NORMAL &&
+        (isspace(*l->cursor) || *l->cursor == '|')) {
       break;
     }
-
-    token[tlen++] = *p->cursor++;
+    tstr[tlen++] = *l->cursor++;
   }
 
-  if (p->state == STATE_IN_QUOTE) {
+  if (l->state == STATE_IN_QUOTE) {
     fprintf(stderr, "unclosed \"");
   }
 
-  token[tlen] = '\0';
-  return token;
+  tstr[tlen] = '\0';
+
+  return token_new(TOKEN_WORD, tstr);
 }
 
-char **tokenize(char *line, size_t *arg_count) {
-  struct parser parser = {.line = line, .cursor = line, .state = STATE_NORMAL};
-  char **argv = malloc(sizeof(char *) * 64);
+struct token_list *tokenize(char *line) {
+  struct lexer lexer = {.line = line, .cursor = line, .state = STATE_NORMAL};
 
-  char *token;
-  while ((token = parser_next(&parser)) != NULL) {
-    argv[(*arg_count)++] = token;
+  struct token_list *lst = malloc(sizeof(*lst));
+
+  size_t lst_initcap = 256;
+  lst->count = 0;
+  lst->capacity = lst_initcap;
+  lst->tokens = malloc(sizeof(*lst->tokens) * lst_initcap);
+
+  for (struct token *t; (t = lexer_next(&lexer)) != NULL;
+       token_list_push(lst, t))
+    ;
+
+  // struct token *t;
+  // while ((t = parser_next(&lexer)) != NULL) {
+  //   token_list_push(lst, t);
+  // };
+
+  return lst;
+}
+
+struct ast_node *ast_node_new(enum ast_node_type type) {
+  struct ast_node *n = malloc(sizeof(*n));
+  n->type = type;
+  memset(&n->data, 0, sizeof(n->data));
+  return n;
+}
+
+void ast_free(struct ast_node *node) {
+  switch (node->type) {
+  case AST_NODE_CMD: {
+    size_t i = 0;
+    while (node->data.args[i] != NULL) {
+      free(node->data.args[i++]);
+    }
+    free(node->data.args);
+    free(node);
+    break;
+  }
+  case AST_NODE_PIPE:
+    ast_free(node->data.child.left);
+    ast_free(node->data.child.right);
+    free(node);
+    break;
+  }
+}
+
+struct ast_node *parse_command(struct token_list *lst, size_t start,
+                               size_t end) {
+  struct ast_node *n = ast_node_new(AST_NODE_CMD);
+  size_t argc = end - start;
+  n->data.args = malloc(sizeof(char *) * (argc + 1));
+  for (size_t i = 0; i < argc; i++) {
+    n->data.args[i] = strdup(lst->tokens[start + i]->str);
+  }
+  n->data.args[argc] = NULL;
+  return n;
+}
+
+struct ast_node *parse(struct token_list *lst, size_t start, size_t end) {
+  size_t i = start;
+  while (i < lst->count && lst->tokens[i]->type == TOKEN_WORD) {
+    i++;
+  }
+  if (i == lst->count) {
+    return parse_command(lst, start, start + i);
   }
 
-  return argv;
+  struct ast_node *n = ast_node_new(AST_NODE_PIPE);
+  n->data.child.left = parse_command(lst, start, i);
+  n->data.child.right = parse(lst, i + 1, end);
+  return n;
 }
--- parser.h
+++ parser.h
@@ -2,14 +2,60 @@
 
 #include <stdlib.h>
 
-enum parser_state { STATE_NORMAL, STATE_IN_QUOTE };
-
-struct parser {
-  char *line;
-  char *cursor;
-  enum parser_state state;
+enum lexer_state {
+  STATE_NORMAL,
+  STATE_IN_QUOTE,
 };
 
-struct parser *parser_new(char *line);
-void parser_free(struct parser *p);
-char **tokenize(char *line, size_t *arg_count);
+enum token_type {
+  TOKEN_WORD,
+  TOKEN_PIPE,
+};
+
+struct token {
+  char *str;
+  enum token_type type;
+};
+
+void token_free(struct token *token);
+
+struct lexer {
+  char *line;
+  char *cursor;
+  enum lexer_state state;
+};
+
+struct token_list {
+  struct token **tokens;
+  size_t count;
+  size_t capacity;
+};
+
+struct lexer *lexer_new(char *line);
+void lexer_free(struct lexer *p);
+struct token_list *tokenize(char *line);
+
+enum ast_node_type {
+  AST_NODE_CMD,
+  AST_NODE_PIPE,
+};
+
+struct parser {
+  struct token_list *lst;
+  struct token *cursor;
+};
+
+struct ast_node {
+  enum ast_node_type type;
+  union {
+    char **args;
+    struct {
+      struct ast_node *left;
+      struct ast_node *right;
+    } child;
+  } data;
+};
+
+void ast_free(struct ast_node *node);
+
+struct ast_node *parse(struct token_list *lst, size_t start, size_t end);
